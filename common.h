218345e5d6d957d693145d4151badfb180a0e2ae
Ivan Shapovalov <intelfx100@gmail.com>
2014-06-12 01:49:38 +0400

    common: add a pseudo-semaphore implementation.
    Possible operations are ::inc(), ::dec() and ::wait_for_zero().

--- common.h
+++ common.h
@@ -3,6 +3,51 @@
 
 #include "log.h"
 
+#include <mutex>
+#include <condition_variable>
+
+class semaphore
+{
+	std::mutex mutex_;
+	std::condition_variable wait_;
+	int count_;
+
+	typedef std::unique_lock<decltype(mutex_)> lock_guard_t;
+
+public:
+	void inc()
+	{
+		lock_guard_t L (mutex_);
+		++count_;
+	}
+
+	void dec()
+	{
+		lock_guard_t L (mutex_);
+		--count_;
+		wait_.notify_one();
+	}
+
+	void wait_for_zero()
+	{
+		lock_guard_t L (mutex_);
+		wait_.wait (L, [this] () { return !(count_ > 0); });
+	}
+
+	class capture
+	{
+		semaphore& ref_;
+
+	public:
+		capture (semaphore& ref)
+			: ref_ (ref)
+			{ ref_.inc(); }
+
+		~capture()
+			{ ref_.dec(); }
+	};
+};
+
 template <typename T>
 bool bit_enabled (T value, size_t bit)
 {
